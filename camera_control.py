d9850be81308b0ce95bad221815589b8dad976fb
Avery Uslaner <uslaner.avery@gmail.com>
2017-09-29 17:35:46 -0600

    Controlling gphoto2 through command line options rather than python bindings.

--- camera_control.py
+++ camera_control.py
@@ -1,5 +1,18 @@
 from subprocess import call
+import time
 
 
-def capture_image():
+def capture_image(wait=1800):
+    """
+    By default, takes an image every 30 minutes.
+
+    Alternatively, you could use the --interval flag
+    of gphoto2 to control the interval.
+    """
+    time.sleep(wait)
     call(['gphoto2', '--capture-image-and-download'])
+
+
+def main():
+    while True:
+        capture_image()
