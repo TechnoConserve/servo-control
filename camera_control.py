e59f9edc8e371a7c9129e1e6ee017f977f26db34
Avery Uslaner <uslaner.avery@gmail.com>
2017-10-10 19:44:59 -0600

    Filenames now take into account existing files.

--- camera_control.py
+++ camera_control.py
@@ -1,6 +1,7 @@
 #!/home/pi/venv/servo_env/bin/python
 
 from subprocess import call
+import os
 import time
 
 
@@ -17,8 +18,34 @@ def capture_image(count, wait=900):
     time.sleep(wait)
 
 
-def main():
+def get_count():
+    """
+    Finds and returns the number for the next image filename based
+    on existing files.
+
+    The image directory here may need to be modified if you would
+    prefer to save images elsewhere.
+    """
     count = 0
+    # Iterate through the list of files
+    for file in os.listdir('/home/pi/images'):
+        # Get the string representation of the number in the filename
+        num_str = file.split('.')[0]
+        # Convert to an actual number
+        num = int(num_str)
+        # If the current filename is a larger number than the current
+        # count variable, assign that value to the count variable
+        if num > count:
+            count = num
+
+    # Add one to the count variable so the next filename doesn't
+    # Overwrite an existing file.
+    count += 1
+    return count
+
+
+def main():
+    count = get_count()
     while True:
         capture_image(count)
         count += 1
